4ea7e7db13d3205ce873651274c68d13125c5cc8
Eduardo Alves <edu.alves88@outlook.com>
2021-09-02 18:28:50 -0300

    Add project files.

---
+++ ExerciciosEstruturaRepetitivaFOR/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExerciciosEstruturaRepetitivaFOR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercícios de repetição FOR!");
+        }
+    }
+}
